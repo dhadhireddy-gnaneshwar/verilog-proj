cc25a953b07aa12e72d0b7c00422df0c14ae0d4a
dhadhireddy-gnaneshwar <116159177+dhadhireddy-gnaneshwar@users.noreply.github.com>
2023-09-21 21:59:14 +0530

    Update DEBONCE.sv

--- srcs/DEBONCE.sv
+++ srcs/DEBONCE.sv
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 30.07.2023 12:51:52
-// Design Name: 
-// Module Name: DEBONCE
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module DEBONCE(
